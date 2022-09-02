79afa35a5e871bfa4c607fc873e00ba7bc2fc780
swikot <swikot@gmail.com>
2022-09-02 21:54:44 +0600

    Added Boiler Plate

--- JPA/src/main/java/com/example/repository/EmployeeRepository.java
+++ JPA/src/main/java/com/example/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package com.example.repository;
 
-import entity.Employee;
+import com.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
