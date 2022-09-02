978449a778881399f43a78e22b667f15288dfd49
swikot <swikot@gmail.com>
2022-09-02 23:49:54 +0600

    ErrorPage Implementation

--- JPA/src/main/java/com/example/JpaApplication.java
+++ JPA/src/main/java/com/example/JpaApplication.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
